0004aaa5c21860a1ca569443bfbea0790633fc46
Yasir Ali <sdr.yasir@gmail.com>
2024-02-13 01:56:43 -0800

    removed the credit text from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <nav className="navbar bg-body-tertiary">
       <div className="container-fluid">
         <span className="navbar-text">
-          Designed By: STICS taight by yasir
+          Designed By: STICS
         </span>
       </div>
     </nav>
